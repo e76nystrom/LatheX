83b3906d8368decbb49f9bfecd059a574013b1f9
Eric Nystrom <eric.nystrom@yahoo.com>
2016-05-06 09:53:13 -0400

    "include/xilinxreg.h Modified on 5/6/2016 at 9:53:08 AM"

--- include/xilinxreg.h
+++ include/xilinxreg.h
@@ -16,67 +16,67 @@ enum XILINX
 
  XRDSR,                         /* 0x08 read status register */
 
-// read phase
-
- XRDPSYN,                       /* 0x09 read sync phase val */
- XRDTPHS,                       /* 0x0a read tot phase val */
-
 // phase counter
 
- XLDPHASE,                      /* 0x0b load phase max */
+ XLDPHASE,                      /* 0x09 load phase max */
 
 // load z motion
 
- XLDZFREQ,                      /* 0x0c load z frequency */
- XLDZD,                         /* 0x0d load z initial d */
- XLDZINCR1,                     /* 0x0e load z incr1 */
- XLDZINCR2,                     /* 0x0f load z incr2 */
- XLDZACCEL,                     /* 0x10 load z syn accel */
- XLDZACLCNT,                    /* 0x11 load z syn acl cnt */
- XLDZDIST,                      /* 0x12 load z distance */
- XLDZLOC,                       /* 0x13 load z location */
+ XLDZFREQ,                      /* 0x0a load z frequency */
+ XLDZD,                         /* 0x0b load z initial d */
+ XLDZINCR1,                     /* 0x0c load z incr1 */
+ XLDZINCR2,                     /* 0x0d load z incr2 */
+ XLDZACCEL,                     /* 0x0e load z syn accel */
+ XLDZACLCNT,                    /* 0x0f load z syn acl cnt */
+ XLDZDIST,                      /* 0x10 load z distance */
+ XLDZLOC,                       /* 0x11 load z location */
 
 // load x motion
 
- XLDXFREQ,                      /* 0x14 load x frequency */
- XLDXD,                         /* 0x15 load x initial d */
- XLDXINCR1,                     /* 0x16 load x incr1 */
- XLDXINCR2,                     /* 0x17 load x incr2 */
- XLDXACCEL,                     /* 0x18 load x syn accel */
- XLDXACLCNT,                    /* 0x19 load x syn acl cnt */
- XLDXDIST,                      /* 0x1a load x distance */
- XLDXLOC,                       /* 0x1b load x location */
+ XLDXFREQ,                      /* 0x12 load x frequency */
+ XLDXD,                         /* 0x13 load x initial d */
+ XLDXINCR1,                     /* 0x14 load x incr1 */
+ XLDXINCR2,                     /* 0x15 load x incr2 */
+ XLDXACCEL,                     /* 0x16 load x syn accel */
+ XLDXACLCNT,                    /* 0x17 load x syn acl cnt */
+ XLDXDIST,                      /* 0x18 load x distance */
+ XLDXLOC,                       /* 0x19 load x location */
 
 // read z motion
 
- XRDZSUM,                       /* 0x1c read z sync sum */
- XRDZXPOS,                      /* 0x1d read z sync x pos */
- XRDZYPOS,                      /* 0x1e read z sync y pos */
- XRDZACLSUM,                    /* 0x1f read z acl sum */
- XRDZASTP,                      /* 0x20 read z acl stps */
+ XRDZSUM,                       /* 0x1a read z sync sum */
+ XRDZXPOS,                      /* 0x1b read z sync x pos */
+ XRDZYPOS,                      /* 0x1c read z sync y pos */
+ XRDZACLSUM,                    /* 0x1d read z acl sum */
+ XRDZASTP,                      /* 0x1e read z acl stps */
 
 // read x motion
 
- XRDXSUM,                       /* 0x21 read x sync sum */
- XRDXXPOS,                      /* 0x22 read x sync x pos */
- XRDXYPOS,                      /* 0x23 read x sync y pos */
- XRDXACLSUM,                    /* 0x24 read x acl sum */
- XRDXASTP,                      /* 0x25 read z acl stps */
+ XRDXSUM,                       /* 0x1f read x sync sum */
+ XRDXXPOS,                      /* 0x20 read x sync x pos */
+ XRDXYPOS,                      /* 0x21 read x sync y pos */
+ XRDXACLSUM,                    /* 0x22 read x acl sum */
+ XRDXASTP,                      /* 0x23 read z acl stps */
 
 // read distance
 
- XRDZDIST,                      /* 0x26 read z distance */
- XRDXDIST,                      /* 0x27 read x distance */
+ XRDZDIST,                      /* 0x24 read z distance */
+ XRDXDIST,                      /* 0x25 read x distance */
 
 // read location
 
- XRDZLOC,                       /* 0x28 read z location */
- XRDXLOC,                       /* 0x29 read x location */
+ XRDZLOC,                       /* 0x26 read z location */
+ XRDXLOC,                       /* 0x27 read x location */
 
 // read frequency and state
 
- XRDFREQ,                       /* 0x2a read encoder freq */
- XRDSTATE,                      /* 0x2b read state info */
+ XRDFREQ,                       /* 0x28 read encoder freq */
+ XRDSTATE,                      /* 0x29 read state info */
+
+// read phase
+
+ XRDPSYN,                       /* 0x2a read sync phase val */
+ XRDTPHS,                       /* 0x2b read tot phase val */
 
 // phase limit info
 
