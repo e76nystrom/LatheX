9094a23680c92edbe0dde0f74fb0036c8278e35f
Eric Nystrom <eric.nystrom@yahoo.com>
2016-05-09 18:19:24 -0400

    "src/lathe.c Modified on 5/9/2016 at 6:19:22 PM"

--- src/lathe.c
+++ src/lathe.c
@@ -10,6 +10,7 @@
 #include <math.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <inttypes.h>
 #include "remvar.h"
 #include "runctl.h"
 #include "zcontrol.h"
@@ -622,8 +623,13 @@ void accelSetup(P_ACCEL ac)
 		      ac->intAccel) / 2;
   ac->accelSteps = (int) ((totalSum + totalInc) / (2 * ac->dx));
   if (DBG_SETUP)
+#ifdef WIN32
    printf("accelClocks %d totalSum %lld totalInc %lld accelSteps %d\n", 
 	  ac->accelClocks, totalSum, totalInc, ac->accelSteps);
+#else
+   printf("accelClocks %d totalSum %" PRId64 " totalInc %" PRId64 " accelSteps %d\n", 
+	  ac->accelClocks, totalSum, totalInc, ac->accelSteps);
+#endif
  }
  else
  {
