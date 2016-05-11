9207effbb7714f209323d53ddde6aa5dccd5fbbf
Eric Nystrom <eric.nystrom@yahoo.com>
2016-05-11 17:39:27 -0400

    "include/xilinxbits.h Modified on 5/11/2016 at 5:39:20 PM"

--- include/xilinxbits.h
+++ include/xilinxbits.h
@@ -1,66 +1,66 @@
 
 // z control register
 
-#define ZRESET        (1 << 0)  /* reset flag */
-#define ZSTART        (1 << 1)  /* start z */
-#define ZSRC_SYN      (1 << 2)  /* run z synchronized */
-#define ZSRC_FRQ      (0 << 2)  /* run z from clock source */
-#define ZDIR_IN       (1 << 3)  /* move z in positive dir */
-#define ZDIR_POS      (1 << 3)  /* move z in positive dir */
-#define ZDIR_NEG      (0 << 3)  /* move z in negative dir */
-#define ZSET_LOC      (1 << 4)  /* set z location */
-#define ZBACKLASH     (1 << 5)  /* backlash move no pos upd */
-#define ZWAIT_SYNC    (1 << 6)  /* wait for sync to start */
-#define ZPULS_MULT    (1 << 7)  /* enable pulse multiplier */
-#define ZENC_DIR      (1 << 8)  /* z direction from encoder */
+#define ZRESET        (1 << 0)  /* 0x001 reset flag */
+#define ZSTART        (1 << 1)  /* 0x002 start z */
+#define ZSRC_SYN      (1 << 2)  /* 0x004 run z synchronized */
+#define ZSRC_FRQ      (0 << 2)  /* 0x000 run z from clock source */
+#define ZDIR_IN       (1 << 3)  /* 0x008 move z in positive dir */
+#define ZDIR_POS      (1 << 3)  /* 0x008 move z in positive dir */
+#define ZDIR_NEG      (0 << 3)  /* 0x000 move z in negative dir */
+#define ZSET_LOC      (1 << 4)  /* 0x010 set z location */
+#define ZBACKLASH     (1 << 5)  /* 0x020 backlash move no pos upd */
+#define ZWAIT_SYNC    (1 << 6)  /* 0x040 wait for sync to start */
+#define ZPULS_MULT    (1 << 7)  /* 0x080 enable pulse multiplier */
+#define ZENC_DIR      (1 << 8)  /* 0x100 z direction from encoder */
 
 // x control register
 
-#define XRESET        (1 << 0)  /* x reset */
-#define XSTART        (1 << 1)  /* start x */
-#define XSRC_SYN      (1 << 2)  /* run x synchronized */
-#define XSRC_FRQ      (0 << 2)  /* run x from clock source */
-#define XDIR_IN       (1 << 3)  /* move x in positive dir */
-#define XDIR_POS      (1 << 3)  /* x positive direction */
-#define XDIR_NEG      (0 << 3)  /* x negative direction */
-#define XSET_LOC      (1 << 4)  /* set x location */
-#define XBACKLASH     (1 << 5)  /* x backlash move no pos upd */
+#define XRESET        (1 << 0)  /* 0x001 x reset */
+#define XSTART        (1 << 1)  /* 0x002 start x */
+#define XSRC_SYN      (1 << 2)  /* 0x004 run x synchronized */
+#define XSRC_FRQ      (0 << 2)  /* 0x000 run x from clock source */
+#define XDIR_IN       (1 << 3)  /* 0x008 move x in positive dir */
+#define XDIR_POS      (1 << 3)  /* 0x008 x positive direction */
+#define XDIR_NEG      (0 << 3)  /* 0x000 x negative direction */
+#define XSET_LOC      (1 << 4)  /* 0x010 set x location */
+#define XBACKLASH     (1 << 5)  /* 0x020 x backlash move no pos upd */
 
 // taper control register
 
-#define TENA          (1 << 0)  /* taper enable */
-#define TZ            (1 << 1)  /* one for taper z */
-#define TX            (0 << 1)  /* zero for taper x */
+#define TENA          (1 << 0)  /* 0x001 taper enable */
+#define TZ            (1 << 1)  /* 0x002 one for taper z */
+#define TX            (0 << 1)  /* 0x000 zero for taper x */
 
 // position control register
 
-#define PRESET        (1 << 0)  /* reset position */
-#define PLIMIT        (1 << 1)  /* set flag on limit reached */
-#define PZERO         (1 << 2)  /* set flag on zero reached */
+#define PRESET        (1 << 0)  /* 0x001 reset position */
+#define PLIMIT        (1 << 1)  /* 0x002 set flag on limit reached */
+#define PZERO         (1 << 2)  /* 0x004 set flag on zero reached */
 
 // configuration register
 
-#define ZSTEP_POL     (1 << 0)  /* z step pulse polarity */
-#define ZDIR_POL      (1 << 1)  /* z direction polarity */
-#define XSTEP_POL     (1 << 2)  /* x step pulse polarity */
-#define XDIR_POL      (1 << 3)  /* x direction polarity */
-#define ENC_POL       (1 << 4)  /* encoder dir polarity */
+#define ZSTEP_POL     (1 << 0)  /* 0x001 z step pulse polarity */
+#define ZDIR_POL      (1 << 1)  /* 0x002 z direction polarity */
+#define XSTEP_POL     (1 << 2)  /* 0x004 x step pulse polarity */
+#define XDIR_POL      (1 << 3)  /* 0x008 x direction polarity */
+#define ENC_POL       (1 << 4)  /* 0x010 encoder dir polarity */
 
 // debug control register
 
-#define DBG_ENA       (1 << 0)  /* enable debugging */
-#define DBG_SEL       (1 << 1)  /* select dbg encoder */
-#define DBG_DIR       (1 << 2)  /* debug direction */
-#define DBG_COUNT     (1 << 3)  /* gen count num dbg clks */
-#define DBG_INIT      (1 << 4)  /* init z modules */
-#define DBG_RSYN      (1 << 5)  /* running in sync mode */
-#define DBG_MOVE      (1 << 6)  /* used debug clock for move */
+#define DBG_ENA       (1 << 0)  /* 0x001 enable debugging */
+#define DBG_SEL       (1 << 1)  /* 0x002 select dbg encoder */
+#define DBG_DIR       (1 << 2)  /* 0x004 debug direction */
+#define DBG_COUNT     (1 << 3)  /* 0x008 gen count num dbg clks */
+#define DBG_INIT      (1 << 4)  /* 0x010 init z modules */
+#define DBG_RSYN      (1 << 5)  /* 0x020 running in sync mode */
+#define DBG_MOVE      (1 << 6)  /* 0x040 used debug clock for move */
 
 // status register
 
-#define S_Z_DONE_INT  (1 << 0)  /* z done interrrupt */
-#define S_X_DONE_INT  (1 << 1)  /* x done interrupt */
-#define S_DBG_DONE    (1 << 2)  /* debug done */
-#define S_Z_START     (1 << 3)  /* z start */
-#define S_X_START     (1 << 4)  /* x start */
-#define S_ENC_DIR_IN  (1 << 5)  /* encoder direction in */
+#define S_Z_DONE_INT  (1 << 0)  /* 0x001 z done interrrupt */
+#define S_X_DONE_INT  (1 << 1)  /* 0x002 x done interrupt */
+#define S_DBG_DONE    (1 << 2)  /* 0x004 debug done */
+#define S_Z_START     (1 << 3)  /* 0x008 z start */
+#define S_X_START     (1 << 4)  /* 0x010 x start */
+#define S_ENC_DIR_IN  (1 << 5)  /* 0x020 encoder direction in */
