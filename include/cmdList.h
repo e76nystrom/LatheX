3286ce48d98b0a258b26af8ce3868c4481b263b3
Eric Nystrom <eric.nystrom@yahoo.com>
2016-05-16 06:54:04 -0400

    "include/cmdList.h Modified on 5/16/2016 at 6:54:01 AM"

--- include/cmdList.h
+++ include/cmdList.h
@@ -15,59 +15,60 @@ enum COMMANDS
 
  XMOVE,                         /* 0x07 move x absolute */
  XMOVEREL,                      /* 0x08 move x relative */
- XSTOP,                         /* 0x09 stop x movement */
- XHOME,                         /* 0x0a set current x location as home */
- XSETLOC,                       /* 0x0b set current x location */
- XGOHOME,                       /* 0x0c x go to home position */
+ XJMOV,                         /* 0x09 start x jog */
+ XSTOP,                         /* 0x0a stop x movement */
+ XHOME,                         /* 0x0b set current x location as home */
+ XSETLOC,                       /* 0x0c set current x location */
+ XGOHOME,                       /* 0x0d x go to home position */
 
 // spindle operations
 
- SPINDLE_START,                 /* 0x0d start spindle */
- SPINDLE_STOP,                  /* 0x0e stop spindle */
+ SPINDLE_START,                 /* 0x0e start spindle */
+ SPINDLE_STOP,                  /* 0x0f stop spindle */
 
 // end operations
 
- CMD_PAUSE,                     /* 0x0f pause current operation */
- CMD_RESUME,                    /* 0x10 resume current operation */
- CMD_STOP,                      /* 0x11 stop current operation */
+ CMD_PAUSE,                     /* 0x10 pause current operation */
+ CMD_RESUME,                    /* 0x11 resume current operation */
+ CMD_STOP,                      /* 0x12 stop current operation */
 
 // setup operations
 
- CMD_CLEAR,                     /* 0x12 clear all tables */
- CMD_SETUP,                     /* 0x13 setup everything */
- CMD_SPSETUP,                   /* 0x14 setup spindle */
- CMD_ZSETUP,                    /* 0x15 setup z axis */
- CMD_ZSYNSETUP,                 /* 0x16 setup z axis sync */
- CMD_ZTAPERSETUP,               /* 0x17 setup z axis taper */
- CMD_XSETUP,                    /* 0x18 setup x axis */
- CMD_XSYNSETUP,                 /* 0x19 setup z axis sync */
- CMD_XTAPERSETUP,               /* 0x1a setup z axis taper */
+ CMD_CLEAR,                     /* 0x13 clear all tables */
+ CMD_SETUP,                     /* 0x14 setup everything */
+ CMD_SPSETUP,                   /* 0x15 setup spindle */
+ CMD_ZSETUP,                    /* 0x16 setup z axis */
+ CMD_ZSYNSETUP,                 /* 0x17 setup z axis sync */
+ CMD_ZTAPERSETUP,               /* 0x18 setup z axis taper */
+ CMD_XSETUP,                    /* 0x19 setup x axis */
+ CMD_XSYNSETUP,                 /* 0x1a setup z axis sync */
+ CMD_XTAPERSETUP,               /* 0x1b setup z axis taper */
 
 // state information
 
- READSTAT,                      /* 0x1b read status */
- READISTATE,                    /* 0x1c read states of state machines */
+ READSTAT,                      /* 0x1c read status */
+ READISTATE,                    /* 0x1d read states of state machines */
 
 // load processor and xilinx parameters
 
- LOADVAL,                       /* 0x1d load parameters */
- READVAL,                       /* 0x1e read parameters */
- LOADXREG,                      /* 0x1f load xilinx registers */
- READXREG,                      /* 0x20 read xilinx registers */
+ LOADVAL,                       /* 0x1e load parameters */
+ READVAL,                       /* 0x1f read parameters */
+ LOADXREG,                      /* 0x20 load xilinx registers */
+ READXREG,                      /* 0x21 read xilinx registers */
 
 // move command operations
 
- SENDMOVE,                      /* 0x21 send move command */
- MOVEQUESTATUS,                 /* 0x22 read move queue status */
+ SENDMOVE,                      /* 0x22 send move command */
+ MOVEQUESTATUS,                 /* 0x23 read move queue status */
 
 // location and debug info
 
- READLOC,                       /* 0x23 read location */
- READDBG,                       /* 0x24 read debug message */
- CLRDBG,                        /* 0x25 clear debug message buffer */
+ READLOC,                       /* 0x24 read location */
+ READDBG,                       /* 0x25 read debug message */
+ CLRDBG,                        /* 0x26 clear debug message buffer */
 
 // encoder commands
 
- ENCSTART,                      /* 0x26 encoder start */
- ENCSTOP,                       /* 0x27 encoder stop */
+ ENCSTART,                      /* 0x27 encoder start */
+ ENCSTOP,                       /* 0x28 encoder stop */
 };
