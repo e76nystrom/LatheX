7e5a2a56cbb2552544fc933eb3f3d7edf3ae9e89
Eric Nystrom <eric.nystrom@yahoo.com>
2016-05-16 06:51:26 -0400

    "include/cmdList.h Modified on 5/16/2016 at 6:51:24 AM"

--- include/cmdList.h
+++ include/cmdList.h
@@ -3,70 +3,71 @@ enum COMMANDS
 
 // z motion commands
 
- ZMOVE,                         /* 0x00 start z movement */
- ZJMOV,                         /* 0x01 start z jog */
- ZSTOP,                         /* 0x02 stop z movement */
- ZHOME,                         /* 0x03 set current z location as home */
- ZSETLOC,                       /* 0x04 set current z location */
- ZGOHOME,                       /* 0x05 z go to home position */
+ ZMOVE,                         /* 0x00 move z absolute */
+ ZMOVEREL,                      /* 0x01 move z relative */
+ ZJMOV,                         /* 0x02 start z jog */
+ ZSTOP,                         /* 0x03 stop z movement */
+ ZHOME,                         /* 0x04 set current z location as home */
+ ZSETLOC,                       /* 0x05 set current z location */
+ ZGOHOME,                       /* 0x06 z go to home position */
 
 // x motion commands
 
- XMOVE,                         /* 0x06 start x movement */
- XJMOV,                         /* 0x07 start x jog */
- XSTOP,                         /* 0x08 stop x movement */
- XHOME,                         /* 0x09 set current x location as home */
- XSETLOC,                       /* 0x0a set current x location */
- XGOHOME,                       /* 0x0b x go to home position */
+ XMOVE,                         /* 0x07 move x absolute */
+ XMOVEREL,                      /* 0x08 move x relative */
+ XSTOP,                         /* 0x09 stop x movement */
+ XHOME,                         /* 0x0a set current x location as home */
+ XSETLOC,                       /* 0x0b set current x location */
+ XGOHOME,                       /* 0x0c x go to home position */
 
 // spindle operations
 
- SPINDLE_START,                 /* 0x0c start spindle */
- SPINDLE_STOP,                  /* 0x0d stop spindle */
+ SPINDLE_START,                 /* 0x0d start spindle */
+ SPINDLE_STOP,                  /* 0x0e stop spindle */
 
 // end operations
 
- CMD_PAUSE,                     /* 0x0e pause current operation */
- CMD_RESUME,                    /* 0x0f resume current operation */
- CMD_STOP,                      /* 0x10 stop current operation */
+ CMD_PAUSE,                     /* 0x0f pause current operation */
+ CMD_RESUME,                    /* 0x10 resume current operation */
+ CMD_STOP,                      /* 0x11 stop current operation */
 
 // setup operations
 
- CMD_CLEAR,                     /* 0x11 clear all tables */
- CMD_SETUP,                     /* 0x12 setup everything */
- CMD_SPSETUP,                   /* 0x13 setup spindle */
- CMD_ZSETUP,                    /* 0x14 setup z axis */
- CMD_ZSYNSETUP,                 /* 0x15 setup z axis sync */
- CMD_ZTAPERSETUP,               /* 0x16 setup z axis taper */
- CMD_XSETUP,                    /* 0x17 setup x axis */
- CMD_XSYNSETUP,                 /* 0x18 setup z axis sync */
- CMD_XTAPERSETUP,               /* 0x19 setup z axis taper */
+ CMD_CLEAR,                     /* 0x12 clear all tables */
+ CMD_SETUP,                     /* 0x13 setup everything */
+ CMD_SPSETUP,                   /* 0x14 setup spindle */
+ CMD_ZSETUP,                    /* 0x15 setup z axis */
+ CMD_ZSYNSETUP,                 /* 0x16 setup z axis sync */
+ CMD_ZTAPERSETUP,               /* 0x17 setup z axis taper */
+ CMD_XSETUP,                    /* 0x18 setup x axis */
+ CMD_XSYNSETUP,                 /* 0x19 setup z axis sync */
+ CMD_XTAPERSETUP,               /* 0x1a setup z axis taper */
 
 // state information
 
- READSTAT,                      /* 0x1a read status */
- READISTATE,                    /* 0x1b read states of state machines */
+ READSTAT,                      /* 0x1b read status */
+ READISTATE,                    /* 0x1c read states of state machines */
 
 // load processor and xilinx parameters
 
- LOADVAL,                       /* 0x1c load parameters */
- READVAL,                       /* 0x1d read parameters */
- LOADXREG,                      /* 0x1e load xilinx registers */
- READXREG,                      /* 0x1f read xilinx registers */
+ LOADVAL,                       /* 0x1d load parameters */
+ READVAL,                       /* 0x1e read parameters */
+ LOADXREG,                      /* 0x1f load xilinx registers */
+ READXREG,                      /* 0x20 read xilinx registers */
 
 // move command operations
 
- SENDMOVE,                      /* 0x20 send move command */
- MOVEQUESTATUS,                 /* 0x21 read move queue status */
+ SENDMOVE,                      /* 0x21 send move command */
+ MOVEQUESTATUS,                 /* 0x22 read move queue status */
 
 // location and debug info
 
- READLOC,                       /* 0x22 read location */
- READDBG,                       /* 0x23 read debug message */
- CLRDBG,                        /* 0x24 clear debug message buffer */
+ READLOC,                       /* 0x23 read location */
+ READDBG,                       /* 0x24 read debug message */
+ CLRDBG,                        /* 0x25 clear debug message buffer */
 
 // encoder commands
 
- ENCSTART,                      /* 0x25 encoder start */
- ENCSTOP,                       /* 0x26 encoder stop */
+ ENCSTART,                      /* 0x26 encoder start */
+ ENCSTOP,                       /* 0x27 encoder stop */
 };
